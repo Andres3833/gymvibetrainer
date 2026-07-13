16314d39fbdff47fe408464d4f2774085261beb1
AlexPp15 <alejandroppe15@gmail.com>
2026-07-13 14:18:25 -0600

    corregido

--- script.js
+++ script.js
@@ -32,12 +32,12 @@ const rutinas = {
         nombre:"Empuje",
 
         ejercicios:[
-            {nombre:"Press de banca",series:4,repeticiones:10-12,descanso:60,peso:""},
-            {nombre:"Press inclinado",series:4,repeticiones:10-12,descanso:60,peso:""},
-            {nombre:"Aperturas",series:4,repeticiones:10-12,descanso:60,peso:""},
-            {nombre:"Press militar",series:4,repeticiones:10-12,descanso:60,peso:""},
-            {nombre:"Laterales",series:4,repeticiones:10-12,descanso:60,peso:""},
-            {nombre:"Tríceps polea",series:4,repeticiones:10-12,descanso:60,peso:""}
+            {nombre:"Press de banca",series:4,repeticiones:"10-12",descanso:60,peso:""},
+            {nombre:"Press inclinado",series:4,repeticiones:"10-12",descanso:60,peso:""},
+            {nombre:"Aperturas",series:4,repeticiones:"10-12",descanso:60,peso:""},
+            {nombre:"Press militar",series:4,repeticiones:"10-12",descanso:60,peso:""},
+            {nombre:"Laterales",series:4,repeticiones:"10-12",descanso:60,peso:""},
+            {nombre:"Tríceps polea",series:4,repeticiones:"10-12",descanso:60,peso:""}
         ]
     },
 
@@ -45,12 +45,12 @@ const rutinas = {
         nombre:"Jalón",
 
         ejercicios:[
-            {nombre:"Dominadas",series:4,repeticiones:10-12,descanso:60,peso:""},
-            {nombre:"Remo",series:4,repeticiones:10-12,descanso:60,peso:""},
-            {nombre:"Pullover",series:4,repeticiones:10-12,descanso:60,peso:""},
-            {nombre:"Curl barra",series:4,repeticiones:10-12,descanso:60,peso:""},
-            {nombre:"Martillo",series:4,repeticiones:10-12,descanso:60,peso:""},
-            {nombre:"Face Pull",series:4,repeticiones:10-12,descanso:60,peso:""}
+            {nombre:"Dominadas",series:4,repeticiones:"10-12",descanso:60,peso:""},
+            {nombre:"Remo",series:4,repeticiones:"10-12",descanso:60,peso:""},
+            {nombre:"Pullover",series:4,repeticiones:"10-12",descanso:60,peso:""},
+            {nombre:"Curl barra",series:4,repeticiones:"10-12",descanso:60,peso:""},
+            {nombre:"Martillo",series:4,repeticiones:"10-12",descanso:60,peso:""},
+            {nombre:"Face Pull",series:4,repeticiones:"10-12",descanso:60,peso:""}
         ]
     },
 
@@ -58,12 +58,12 @@ const rutinas = {
         nombre:"Pierna",
 
         ejercicios:[
-            {nombre:"Sentadilla",series:4,repeticiones:10-12,descanso:90,peso:""},
-            {nombre:"Prensa",series:4,repeticiones:10-12,descanso:90,peso:""},
-            {nombre:"Peso muerto rumano",repeticiones:10-12,series:4,descanso:90,peso:""},
-            {nombre:"Extensión",series:4,repeticiones:10-12,descanso:60,peso:""},
-            {nombre:"Femoral",series:4,repeticiones:10-12,descanso:60,peso:""},
-            {nombre:"Pantorrilla",series:4,repeticiones:10-12,descanso:60,peso:""}
+            {nombre:"Sentadilla",series:4,repeticiones:"10-12",descanso:90,peso:""},
+            {nombre:"Prensa",series:4,repeticiones:"10-12",descanso:90,peso:""},
+            {nombre:"Peso muerto rumano",repeticiones:"10-12",series:4,descanso:90,peso:""},
+            {nombre:"Extensión",series:4,repeticiones:"10-12",descanso:60,peso:""},
+            {nombre:"Femoral",series:4,repeticiones:"10-12",descanso:60,peso:""},
+            {nombre:"Pantorrilla",series:4,repeticiones:"10-12",descanso:60,peso:""}
         ]
     }
 
